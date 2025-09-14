6bd937d09691f84800e4d8ca1bdcd5c8f82af97b
Elliot Alberto Gutierrez Cisneros <elliotgtz06@gmail.com>
2025-09-13 20:01:24 -0600

    tareas_nuevas

--- Clases/juego/lineas dañinas.py
+++ Clases/juego/lineas dañinas.py
@@ -42,7 +42,7 @@ class Jugador:
         self.y = 300
         self.ancho = 25
         self.alto = 25
-        self.velocidad = 0.35
+        self.velocidad = 0.50
         self.color = (255, 255, 255)  # Color blanco
 
     #declaración de funciones
@@ -208,3 +208,21 @@ while ejecutando:
 
 
 pygame.quit()
+
+"""
+tarea pendiente:
+- cuando una linea real descuente una vida, debe desaparecer y compensarlo con una nueva linea fantasma
+-ajustes en dificultas:
+    - cada 30 segundos, aumenta la velocidad de creaación de las lineas fantasma + 20 milisegundos
+    - cada 60 segundos, aumenta la velocidad de crecimiento de las lineas fantasma + 20 milisegundos
+    - al minuto 2, que empiesen a generarse 2 lineas fantasma a la vez
+    y que se generen lineas verticales y horizontales
+
+- mejorar la estetica del juegos:
+    - que las lineas reales desaparescan suavemente is se tocan
+            - que el fondo flashe en rojo cuando se pierde una vida
+            y flashe blanco cuando se genere una linea real
+    - que el jugador parpadee cuando se toque una linea real
+    y que se vaya fracturando (haciendo grietas)
+    - un oscurecer mas suave 
+"""
