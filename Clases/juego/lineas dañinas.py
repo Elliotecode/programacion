79d7d79f267bfb1129ef1ca91359b3a614db8839
Elliot Alberto Gutierrez Cisneros <elliotgtz06@gmail.com>
2025-11-01 20:21:20 -0600

    nuevas tareas super actualizadas

--- Clases/juego/lineas dañinas.py
+++ Clases/juego/lineas dañinas.py
@@ -146,7 +146,7 @@ class Linea_Real:
     def __init__(self, linea_fantasma_ref):
         self.x = linea_fantasma_ref.x
         self.y = linea_fantasma_ref.y
-        self.ancho = linea_fantasma_ref.ancho #max(linea_fantasma_ref.ancho, 50) #linea_fantasma_1.ancho
+        self.ancho = linea_fantasma_ref.ancho
         self.alto = linea_fantasma_ref.alto
         self.color = [255, 255, 255]  # Color blanco
         self.velocidad = 5
@@ -271,13 +271,13 @@ pygame.quit()
 tarea pendiente:
 
 -ajustes en dificultas:
-    - cada 30 segundos, aumenta la velocidad de creaación de las lineas fantasma + 20 milisegundos
+    - cada 30 segundos, aumenta la velocidad de creación de las lineas fantasma + 20 milisegundos
     - cada 60 segundos, aumenta la velocidad de crecimiento de las lineas fantasma + 20 milisegundos
     y que se generen lineas verticales y horizontales
+        -pensar la estrucura de los niveles
 
 - ajustes del fondo:
     - reincorporar el efecto de flash cada que se crea una linea real
-    - agregar transparencia a las lineas reales
+        - agregar transparencia a las lineas reales
 
-- arreglar el bug de crecimiento de las lineas fantasmas deficiente
 """
