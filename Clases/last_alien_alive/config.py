d67eaf1cc4d8e5a4ff25e863735d58a79d3c0478
Elliot Alberto Gutierrez Cisneros <elliotgtz06@gmail.com>
2026-04-18 19:33:28 -0600

    establizando main

--- Clases/last_alien_alive/config.py
+++ Clases/last_alien_alive/config.py
@@ -23,8 +23,8 @@ INTERVALO_ANIMACION_JUGADOR = 100  #  de animación del jugador en milisegundos
 TIEMPO_LINEA_REAL = 5000
 
 #rutas de assets
-FONDO = pygame.image.load("Clases/juego/assets/espacioooooo.jpg").convert()
-jugador = pygame.image.load("Clases/juego/assets/alien_blano.png").convert_alpha()
+FONDO = pygame.image.load("assets/espacioooooo.jpg").convert()
+jugador = pygame.image.load("assets/alien_blano.png").convert_alpha()
 jugador = pygame.transform.scale(jugador, (30, 30))  # Nuevo tamaño: 25x25 píxeles
-jugador_caminando = pygame.image.load("Clases/juego/assets/alien_move.png").convert_alpha()
+jugador_caminando = pygame.image.load("assets/alien_move.png").convert_alpha()
 jugador_caminando = pygame.transform.scale(jugador_caminando, (30, 30))  # Nuevo tamaño: 25x25 píxeles
