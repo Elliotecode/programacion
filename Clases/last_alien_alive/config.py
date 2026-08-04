8e4f27706bcb42d5eb2aa66463abaca57c788af5
elliotcodem <elliotgtz06@gmail.com>
2026-08-03 19:32:27 -0500

    animacion exitosa

--- Clases/last_alien_alive/config.py
+++ Clases/last_alien_alive/config.py
@@ -27,22 +27,31 @@ jugador_moviendose = False
 
 #rutas de assets
 
-FONDO = pygame.image.load("assets/espaciooooooooo.png").convert()
+FONDO = pygame.image.load("/home/elliot/Escritorio/Elliot/cosas_principales/programacion/Clases/last_alien_alive/assets/Espaciooooooooo.png").convert()
 
-sonido_laser = pygame.mixer.Sound("assets/starwars.mp3")
-sonido_colision = pygame.mixer.Sound("assets/golpe_por_sable.mp3")
-sonido_muerte = pygame.mixer.Sound("assets/destruccion.mp3")
-sonido_movimiento = pygame.mixer.Sound("assets/huida.mp3")
+sonido_laser = pygame.mixer.Sound("/home/elliot/Escritorio/Elliot/cosas_principales/programacion/Clases/last_alien_alive/assets/starwars.mp3")
+sonido_colision = pygame.mixer.Sound("/home/elliot/Escritorio/Elliot/cosas_principales/programacion/Clases/last_alien_alive/assets/golpe_por_sable.mp3")
+sonido_muerte = pygame.mixer.Sound("/home/elliot/Escritorio/Elliot/cosas_principales/programacion/Clases/last_alien_alive/assets/destruccion.mp3")
+sonido_movimiento = pygame.mixer.Sound("/home/elliot/Escritorio/Elliot/cosas_principales/programacion/Clases/last_alien_alive/assets/huida.mp3")
 
-jugador = pygame.image.load("assets/alien_blano.png").convert_alpha()
+jugador = pygame.image.load("/home/elliot/Escritorio/Elliot/cosas_principales/programacion/Clases/last_alien_alive/assets/alien_blano.png").convert_alpha()
 jugador = pygame.transform.scale(jugador, (30, 30))  # Nuevo tamaño: 25x25 píxeles
-jugador_caminando = pygame.image.load("assets/alien_move.png").convert_alpha()
+jugador_caminando = pygame.image.load("/home/elliot/Escritorio/Elliot/cosas_principales/programacion/Clases/last_alien_alive/assets/alien_move.png").convert_alpha()
 jugador_caminando = pygame.transform.scale(jugador_caminando, (30, 30))  # Nuevo tamaño: 25x25 píxeles
-exp1 = pygame.image.load("assets/explosion/exp1.png").convert_alpha()
-exp2 = pygame.image.load("assets/explosion/exp2.png").convert_alpha()
-exp3 = pygame.image.load("assets/explosion/exp3.png").convert_alpha()
-exp4 = pygame.image.load("assets/explosion/exp4.png").convert_alpha()
-exp5 = pygame.image.load("assets/explosion/exp5.png").convert_alpha()
-exp6 = pygame.image.load("assets/explosion/exp6.png").convert_alpha()
-exp7 = pygame.image.load("assets/explosion/exp7.png").convert_alpha()
-exp8 = pygame.image.load("assets/explosion/exp8.png").convert_alpha()
+exp1 = pygame.image.load("/home/elliot/Escritorio/Elliot/cosas_principales/programacion/Clases/last_alien_alive/assets/explosion/exp1.png").convert_alpha()
+exp1_redi = pygame.transform.scale(exp1, (30, 30)) #ancho, alto
+exp2 = pygame.image.load("/home/elliot/Escritorio/Elliot/cosas_principales/programacion/Clases/last_alien_alive/assets/explosion/exp2.png").convert_alpha()
+exp2_redi = pygame.transform.scale(exp2, (50, 50)) #ancho, alto
+exp3 = pygame.image.load("/home/elliot/Escritorio/Elliot/cosas_principales/programacion/Clases/last_alien_alive/assets/explosion/exp3.png").convert_alpha()
+exp3_redi = pygame.transform.scale(exp3, (70, 70)) #ancho, alto
+exp4 = pygame.image.load("/home/elliot/Escritorio/Elliot/cosas_principales/programacion/Clases/last_alien_alive/assets/explosion/exp4.png").convert_alpha()
+exp4_redi = pygame.transform.scale(exp4, (110, 110)) #ancho, alto
+exp5 = pygame.image.load("/home/elliot/Escritorio/Elliot/cosas_principales/programacion/Clases/last_alien_alive/assets/explosion/exp5.png").convert_alpha()
+exp5_redi = pygame.transform.scale(exp5, (130, 130)) #ancho, alto
+exp6 = pygame.image.load("/home/elliot/Escritorio/Elliot/cosas_principales/programacion/Clases/last_alien_alive/assets/explosion/exp6.png").convert_alpha()
+exp6_redi = pygame.transform.scale(exp6, (150, 150)) #ancho, alto
+exp7 = pygame.image.load("/home/elliot/Escritorio/Elliot/cosas_principales/programacion/Clases/last_alien_alive/assets/explosion/exp7.png").convert_alpha()
+exp7_redi = pygame.transform.scale(exp7, (170, 170)) #ancho, alto
+exp8 = pygame.image.load("/home/elliot/Escritorio/Elliot/cosas_principales/programacion/Clases/last_alien_alive/assets/explosion/exp8.png").convert_alpha()
+exp8_redi = pygame.transform.scale(exp8, (190, 190)) #ancho, alto
+explosion_frames = [exp1, exp2, exp3, exp4, exp5, exp6, exp7, exp8]
--- Clases/last_alien_alive/main.py
+++ Clases/last_alien_alive/main.py
@@ -136,16 +136,30 @@ while ejecutando:
             print("pocision final", final_x - 10, final_y - 10)
 
         if game_over == True:
-            exp1_redi = pygame.transform.scale(exp1, (30, 30)) #ancho, alto
-            pantalla.blit(exp1_redi, (final_x - 15, final_y - 15))
-            if tiempo_actual - hora_de_muerte >= 200:
-                exp2_redi = pygame.transform.scale(exp2, (50, 50)) #ancho, alto
+            if tiempo_actual - hora_de_muerte <= 100:
+                pantalla.blit(exp1_redi, (final_x - 15, final_y - 15))
+            if tiempo_actual - hora_de_muerte >= 100 and tiempo_actual - hora_de_muerte <= 200:
                 pantalla.blit(exp2_redi, (final_x - 25, final_y - 25))
-            """
-            if tiempo_actual - game_over_start_time >= 2000:
-                exp3_redi = pygame.transform.scale(exp3, (75, 75)) #ancho, alto
-                pantalla.blit(exp3_redi, (final_x - 75, final_y - 75))
-            """
+            if tiempo_actual - hora_de_muerte >= 200 and tiempo_actual - hora_de_muerte <= 300:
+                pantalla.blit(exp3_redi, (final_x - 35, final_y - 35))
+            if tiempo_actual - hora_de_muerte >= 300 and tiempo_actual - hora_de_muerte <= 400:
+                pantalla.blit(exp4_redi, (final_x - 55, final_y - 55))
+            if tiempo_actual - hora_de_muerte >= 400 and tiempo_actual - hora_de_muerte <= 500:
+                pantalla.blit(exp5_redi, (final_x - 65, final_y - 65)) 
+            if tiempo_actual - hora_de_muerte >= 500 and tiempo_actual - hora_de_muerte <= 600:
+                pantalla.blit(exp6_redi, (final_x - 75, final_y - 75))
+            if tiempo_actual - hora_de_muerte >= 600 and tiempo_actual - hora_de_muerte <= 700:
+                pantalla.blit(exp7_redi, (final_x - 85, final_y - 85))
+            if tiempo_actual - hora_de_muerte >= 700 and tiempo_actual - hora_de_muerte <= 800:
+                pantalla.blit(exp8_redi, (final_x - 95, final_y - 95))
+
+
+                """            
+                if tiempo_actual - game_over_start_time >= 2000:
+                    exp3_redi = pygame.transform.scale(exp3, (75, 75)) #ancho, alto
+                    pantalla.blit(exp3_redi, (final_x - 75, final_y - 75))
+                """         
+            
         """   
         x + a = derecha
         x - a = izquierda
@@ -169,14 +183,12 @@ while ejecutando:
     
 
     else:
-        mensaje = FUENTE.render("Juego Terminado", True, (255, 0, 0))
+        mensaje = FUENTE.render("Juego Terminado", True, (128, 0, 0))
         pantalla.blit(mensaje, (ANCHO_PANTALLA // 2 - mensaje.get_width() // 2, ALTO_PANTALLA // 2 - mensaje.get_height() // 2))
 
         #actualizar pantalla
     pygame.display.flip()
     pygame.time.delay(6)  # 10 milisegundos
-
-
 pygame.quit()
 
 """
